8c43b73c2bf552b3d7c1df1fa685542d3de4550b
Jack Rosa <jackrosa183@gmail.com>
2022-09-15 10:22:16 -0400

    post partials

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -5,12 +5,19 @@ class PostTest < ActiveSupport::TestCase
   setup do
     @user = User.create(email: "eric@example.com", password: "password")
   end
-  
+
+  test "Posts are correctly published" do
+    post_1 = Post.create(title: "published", content: "published content", created_at: 5.minutes.ago, user: @user, publish_date: Date.current.tomorrow)
+    post_2 = Post.create(title: "second title", content: "first content", created_at: 5.minutes.ago, user: @user, publish_date: Date.current.tomorrow)
+    
+    assert_not_equal @user.posts.published, [post_1, post_2]
+  end
+
   test "Posts are in descending order" do
     
-    post_3 = Post.create(title: "first title", content: "first content", created_at: 10.minutes.ago, user: @user)
-    post_1 = Post.create(title: "third title", content: "third content", created_at: 3.minutes.ago, user: @user)
-    post_2 = Post.create(title: "second title", content: "second title", created_at: 5.minutes.ago, user: @user)
+    post_3 = Post.create(title: "first title", content: "first content", created_at: 10.minutes.ago, user: @user, publish_date: Date.current.yesterday)
+    post_1 = Post.create(title: "third title", content: "third content", created_at: 3.minutes.ago, user: @user, publish_date: Date.current.yesterday)
+    post_2 = Post.create(title: "second title", content: "second title", created_at: 5.minutes.ago, user: @user, publish_date: Date.current.yesterday)
 
     assert_equal @user.posts.ordered, [post_1, post_2, post_3]
  end 
@@ -22,7 +29,7 @@ class PostTest < ActiveSupport::TestCase
 
   assert post_1.errors.where(:user, :blank).present? 
 
-  post_1 = Post.new(title: "third title", content: "third content", created_at: 3.minutes.ago, user: @user)
+  post_1 = Post.new(title: "third title", content: "third content", created_at: 3.minutes.ago, user: @user, publish_date: Date.current.yesterday)
   assert post_1.valid?
   assert_not post_1.errors.where(:user, :blank).present? 
  end
