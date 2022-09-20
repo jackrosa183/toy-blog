bc2a07c2c84245976ca25bee926e60f4d6fceaa9
Jack Rosa <jackrosa183@gmail.com>
2022-09-20 16:14:21 -0400

    refactored omniauth middleware build

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,19 +1,15 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter,
+  provider(:twitter,
   Rails.application.credentials.dig(:twitter, :api_key), 
-  Rails.application.credentials.dig(:twitter, :api_secret)
+  Rails.application.credentials.dig(:twitter, :api_secret))
 
-end
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,
+  provider(:facebook,
   Rails.application.credentials.dig(:facebook, :api_id),
-  Rails.application.credentials.dig(:facebook, :api_secret)
+  Rails.application.credentials.dig(:facebook, :api_secret))
 
-end
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin,
+  provider(:linkedin,
   Rails.application.credentials.dig(:linkedin, :client_id),
   Rails.application.credentials.dig(:linkedin, :client_secret),
-  scope: 'r_emailaddress'
+  scope: 'r_liteprofile')
 end
+
