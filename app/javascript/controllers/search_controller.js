a84d07f5a79c6b93d7b43996c079e669413e8338
Jack Rosa <jackrosa183@gmail.com>
2022-09-22 15:55:06 -0400

    tag searches now show up in new post

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,14 +1,27 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = [ "tag_titles", "results" ]
+  static targets = [ "query", "results" ]
   static values = { url: String }
 
   connect() {
     console.log("search is totally connected")
   }
-  fetchResults(){
+  fetchResults(e){
+    const url = new URL(this.urlValue)
+    console.log(e.target.value)
+    //console.log(this.queryTarget + "!!!!!!!!!!!")
+    url.searchParams.append('query', e.target.value)
     console.log("Fetching Results")
+    console.log(url)
+
+    this.abortController = new AbortController()
+    fetch(url, {signal: this.abortController.signal })
+      .then(response => response.text())
+      .then(html => {
+        this.resultsTarget.innerHTML = html
+      })
+      .catch(() => {})
   }
   reset() {
     console.log("Resetting")
