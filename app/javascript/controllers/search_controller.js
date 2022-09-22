5c3cdc6557ff5dca970dc7bc046f5ebc0af623a9
Jack Rosa <jackrosa183@gmail.com>
2022-09-22 16:32:14 -0400

    cleaning up controller

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -9,17 +9,13 @@ export default class extends Controller {
   }
   fetchResults(e){
     const url = new URL(this.urlValue)
-    console.log(e.target.value)
+
     if(e.target.value == ""){
       this.resultsTarget.innerHTML = ""
       e.target.value = ""
       return
     }
-    //console.log(this.queryTarget + "!!!!!!!!!!!")
     url.searchParams.append('query', e.target.value)
-    console.log("Fetching Results")
-    console.log(url)
-    //response includes heading
     this.abortController = new AbortController()
     fetch(url, {signal: this.abortController.signal })
       .then(response => response.text())
