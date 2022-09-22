e8995416add07e1c05fdfb620218fba4f767e15e
Jack Rosa <jackrosa183@gmail.com>
2022-09-22 13:37:26 -0400

    stimulus controller properly connects

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,5 +1,9 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  
+  static targets = [ "query", "results" ]
+  static values = { url: String }
+  connect() {
+    console.log("search is totally connected")
+  }
 }
