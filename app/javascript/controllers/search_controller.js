6204094b6d3a62cc670072efb1a2f32e2362c209
Jack Rosa <jackrosa183@gmail.com>
2022-09-23 16:34:47 -0400

    working on adding clickabel list elements for added tag titles

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,25 +1,26 @@
 import { Controller } from "@hotwired/stimulus";
+import { List } from "immutable";
 
 export default class extends Controller {
-  static targets = [ "query", "results", "title" ]
+  static targets = [ "query", "results", "tags"]
   static values = { url: String }
 
   connect() {
     console.log("search is totally connected")
   }
-  updateInput() {
-    console.log("update")
-  }
-  
+
   fetchResults(e){
     if(e.keyCode == 32 ){
-      console.log("space")
+      var li = document.createElement("a")
+      li.innerHTML = this.queryTarget.value
+      li.className = "btn-secondary-smaller"
+      this.tagsTarget.append(li)
+      this.reset()
     }
     const url = new URL(this.urlValue)
 
     if(e.target.value == ""){
-      this.resultsTarget.innerHTML = ""
-      e.target.value = ""
+      this.reset()
       return
     }
     url.searchParams.append('query', e.target.value)
@@ -35,7 +36,26 @@ export default class extends Controller {
     // debugger
     console.log("clicked on " + e.target.dataset.title)
     // this.updateInput()
-    this.queryTarget.value = e.target.dataset.title + " "
+    this.queryTarget.value = ""
 
+    var li = document.createElement("a")
+    li.innerHTML = e.target.dataset.title + " "
+    li.className = "btn-secondary-smaller"
+
+    if(this.tagsTarget.innerHTML.includes(li)){
+      console.log("already there")
+      this.tagsTarget.append(li)
+    }
+    else{
+      this.reset()
+      return 
+    }
+    
+    // e.target.dataset.title + " "
+    
+  }
+  reset(){
+    this.queryTarget.value = ""
+    this.resultsTarget.innerHTML = "" 
   }
 }
