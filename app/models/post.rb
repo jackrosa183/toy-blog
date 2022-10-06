f792ccf2ead26fcaff5184834a7356ca3b67e971
Jack Rosa <jackrosa183@gmail.com>
2022-10-06 15:55:38 -0400

    json search post title

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   validates :publish_date, presence: true
   validates :post_tags, length: { maximum: 5, too_long: "5 tags is the maximum allowed" }
   # validates :tags, length: { maximum: 5 }
-
+  scope :containing, -> (query) { where(arel_table[:title].matches("%#{query}%")) }
   scope :ordered, -> { order(created_at: :desc) }
   scope :published, -> { where('publish_date <= ?', Date.current).order(publish_date: :desc)}
   scope :tagged_with, ->(title) { joins(:tags).where(tags: {title: title}) }
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   get '/index_drafts', to: "posts#index_drafts"
   get '/index_tags', to: "posts#index_tags"
+  get '/find_posts/', to: "searches#find_posts", as: "find_posts"
 
   get "users/:id/edit_avatar", to: "users#edit_avatar", as: "edit_user_avatar"
   post "users/:id/update_avatar", to: "users#update_avatar", as: "update_user_avatar"
