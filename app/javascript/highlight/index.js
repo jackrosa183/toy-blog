21ee651cd6986df4a83e0d5633b7416a14d4320d
Jack Rosa <jackrosa183@gmail.com>
2022-10-11 15:39:36 -0400

    added syntax highlighting for extra languages

--- app/javascript/highlight/index.js
+++ app/javascript/highlight/index.js
@@ -1,7 +1,7 @@
 import hljs from 'highlight.js'
 import 'highlight.js/styles/monokai.css'
 
-hljs.configure({languages: ['ruby', 'javascript', 'bash'] })
+hljs.configure({languages: ['ruby', 'javascript', 'bash', 'html', 'elixir', 'css'] })
 document.addEventListener("turbo:load", (event) => {
   document.querySelectorAll('pre').forEach((block) => {
     hljs.highlightBlock(block)
