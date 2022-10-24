7665ae1e2deebbf2ef152813aacc6017abbc027d
Jack Rosa <jackrosa183@gmail.com>
2022-10-24 12:08:23 -0400

    added comment maximum length validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :user_id, presence: true
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 800 }
   validates :post_id, presence: true
 
   scope :ordered, -> { order(created_at: :desc) }
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,11 +3,11 @@ require "test_helper"
 class CommentTest < ActiveSupport::TestCase
 
   setup do
+    @user = User.create(email: "eric@example.com", password: "passwword")
+    @post = Post.create(title: "Title", rich_content: "Content", user: @user, publish_date: Date.current.yesterday)
   end
   
   test "Comment post validations" do    
-    @user = User.create(email: "eric@example.com", password: "passwword")
-    @post = Post.create(title: "Title", rich_content: "Content", user: @user, publish_date: Date.current.yesterday)
     comment = Comment.create(content: "comments are cool")
     
     assert comment.invalid?
@@ -16,5 +16,12 @@ class CommentTest < ActiveSupport::TestCase
 
     comment = Comment.create(content: "comments", user_id: @user.id, post_id: @post.id)
     assert comment.valid?
+  end
+
+  test "comment cannot be too large" do 
+      big_string = "a" * 1000
+      comment = Comment.create(content: big_string, user_id: @user.id, post_id: @post_id)
+      assert comment.invalid?
+      assert comment.errors.where(:content, :too_long).present?
   end
 end
