d0bbac5583bd59e0d282866dd29b83616c03d07f
Jack Rosa <jackrosa183@gmail.com>
2022-10-24 11:30:41 -0400

    added top padding to all index posts pages

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,6 @@ class SearchesController < ApplicationController
   def index
     @tags = Tag.containing(params[:query])
     render layout: false
-    @title = params[:query]
     p @title.to_s + "!!!!!!"
   end
 
